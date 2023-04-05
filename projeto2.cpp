cc5dc60b21d3ef0993d3337c995e983b41d03e3c
FabioS <fabalexandre8@gmail.com>
2023-04-05 17:00:03 +0100

    int -> long 200 mooshak

--- projeto2.cpp
+++ projeto2.cpp
@@ -26,10 +26,10 @@ int R; // taxa fixa
  */
 
 vector<vector<int>> V;    // vector que em cada linha representa uma empresa e cada coluna representa um dia
-int profit;               // usado para imprimir o lucro total no final
-vector<vector<int>> dp;   // guardar a info se naquele dia comprou ou vendeu e qual o profit desse caminho
-vector<int> history;      // usado para guardar o historico de compras/vendas ao longo dos dias
-vector<int> history_copy; // usado para duplicar o historico no caso de haver mais alguma possibilidade
+long profit;               // usado para imprimir o lucro total no final
+vector<vector<long>> dp;   // guardar a info se naquele dia comprou ou vendeu e qual o profit desse caminho
+vector<long> history;      // usado para guardar o historico de compras/vendas ao longo dos dias
+vector<long> history_copy; // usado para duplicar o historico no caso de haver mais alguma possibilidade
 int possibilidades;       // guardar quantas possibilidades de compras/vendas dariam o lucro maximo
 
 void writeVector() {
@@ -43,7 +43,7 @@ void writeVector() {
 }
 
 // DP
-int maxProfit(int company, int day, int buy) {
+long maxProfit(int company, int day, int buy) {
 
     if (day == D) {
         return 0;
@@ -53,40 +53,27 @@ int maxProfit(int company, int day, int buy) {
         return dp[day][buy];
     }
 
-    int prof = 0;
+    long prof = 0;
 
     if (buy) {
         prof = max(-V[company][day] * K - R * K + maxProfit(company, day + 1, 0),
                    0 + maxProfit(company, day + 1, 1));
-
-        // if (prof == -V[company][day] * K - R * K + maxProfit(company, day + 1, 0)) {
-        //     cout << "Comprei dia" << day + 1 << " DP" << dp[day][buy] << endl;
-        // } else {
-        //     cout << "Mantive dia" << day + 1 << " DP" << dp[day][buy] << endl;
-        // }
     } else {
         prof = max(V[company][day] * K + maxProfit(company, day + 1, 1),
                    0 + maxProfit(company, day + 1, 0));
-
-        // if (prof == V[company][day] * K + maxProfit(company, day + 1, 1)) {
-        //     cout << "Vendi dia" << day + 1 << " DP" << dp[day][buy] << endl;
-        // } else {
-        //     cout << "Mantive dia" << day + 1 << " DP" << dp[day][buy] << endl;
-        // }
-        // cout << endl;
     }
 
     return dp[day][buy] = prof;
 }
 
 // Bottom Up
-int maxProfit2(int company) {
-    vector<int> next = vector<int>(2, 0);
-    vector<int> cur = vector<int>(2, 0);
+long maxProfit2(int company) {
+    vector<long> next = vector<long>(2, 0);
+    vector<long> cur = vector<long>(2, 0);
 
     next[0] = next[1] = 0;
     for (int day = D - 1; day >= 0; day--) {
-        int prof = 0;
+        long prof = 0;
         for (int buy = 0; buy <= 1; buy++) {
             prof = 0;
             if (buy) {
@@ -115,7 +102,7 @@ void bestSequence(int company) {
     for (int day = 0; day < D; day++) {
 
         if (day != D - 1) {
-            int prof = dp[day][buy];
+            long prof = dp[day][buy];
             if (prof == -V[company][day] * K - R * K + maxProfit(company, day + 1, 0)) {
                 history[day] = K;
                 buy = 0;
@@ -126,7 +113,7 @@ void bestSequence(int company) {
                 history[day] = 0;
             }
         } else {
-            
+
             // tratar do ultimo elemento
             if (buy) {
                 history[day] = 0; // vender todas as ações no último dia
@@ -139,10 +126,10 @@ void bestSequence(int company) {
 
 void task2() {
     for (int i = 0; i < N; i++) {
-        history = vector<int>(D, 0);
+        history = vector<long>(D, 0);
 
         // dp
-        dp = vector<vector<int>>(D, vector<int>(2, -1));
+        dp = vector<vector<long>>(D, vector<long>(2, -1));
         profit = maxProfit(i, 0, 1);
 
         // bottom up
@@ -160,13 +147,39 @@ void task2() {
     }
 }
 
+long maxProfit3(int company) {
+    vector<vector<int>> next = vector<vector<int>>(2, vector<int>(K, 0));
+    vector<vector<int>> cur = vector<vector<int>>(2, vector<int>(K, 0));
+
+    next[0][0] = next[1][0] = 0;
+    for (int day = D - 1; day >= 0; day--) {
+        long prof = 0;
+        for (int buy = 0; buy <= 1; buy++) {
+            prof = 0;
+            for (int k = 0; k < K; k++) {
+                if (k>0) {
+                    prof = max(-V[company][day] * K - R * K + next[0][k],
+                               0 + next[1][k]);
+                } else {
+                    prof = max(V[company][day] * K + next[1][k],
+                               0 + next[0][k]);
+                }
+            }
+            cur[buy][0] = prof;
+        }
+
+        next = cur;
+    }
+    return next[1][K-1];
+}
+
 void task3() {
     for (int i = 0; i < N; i++) {
-        dp = vector<vector<int>>(D, vector<int>(2, -1));
-        history = vector<int>(D, 0);
-        history_copy = vector<int>(D, 0);
-        possibilidades = 0;
-        profit = maxProfit(i, 0, 1);
+        // dp = vector<vector<long>>(D, vector<long>(2, -1));
+        history = vector<long>(D, 0);
+        history_copy = vector<long>(D, 0);
+        possibilidades = 1;
+        profit = maxProfit2(i);
 
         cout << profit << " " << possibilidades << endl;
     }
